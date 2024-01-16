807dd014ae3273cfddf2561c79777280d3aa6658
Bianca Haidau <bianca.haidau@gmail.com>
2024-01-16 14:49:15 +0200

    Creating Window

--- Ping Pong Game/Ping Pong Game.cpp
+++ Ping Pong Game/Ping Pong Game.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+#include <windows.h>
 
-int main()
+bool running = true;
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM WParam, LPARAM LParam)
 {
-    std::cout << "Hello World!\n";
+	LRESULT result = 0;
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+	case WM_DESTROY: {running = false; } 
+	break;
+
+	default:
+		{
+			result = DefWindowProc(hwnd, uMsg, WParam, LParam);
+		}
+	}
+	return result;
 }
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	//Create window 
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+
+	//Register Window
+	RegisterClass(&window_class);
+
+
+	HWND window = CreateWindow(window_class.lpszClassName, "Ping Pong", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	
+	//Simulation
+	while (running)
+	{
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+	}
+}
+
